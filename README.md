9b1e5cb6102ca1ae77798eed6ca71e2b71c843a2
Riccardo Crippa <therickys93@gmail.com>
2015-11-18 11:11:28 +0100

    Added the c version

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This repository shows simple hello world programs of many languages for testing purpose
 
 So many languages are be added after time stay tuned
+
+Added the c version
---
+++ hello-world.c
@@ -0,0 +1,19 @@
+/**
+ * hello-world.c
+ *
+ * Riccardo Crippa
+ * therickys93@gmail.com
+ *
+ * To compile this use:
+ * gcc or cc or clang
+ * for example
+ * gcc -o hello hello-world.c
+ * to run it type
+ * ./hello
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World!!!\n");
+} 
